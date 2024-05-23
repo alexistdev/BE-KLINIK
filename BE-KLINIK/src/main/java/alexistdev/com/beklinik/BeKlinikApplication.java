55118b8f0fcddbdf8ab65e63f832b292c664ac2f
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2024-05-23 20:45:18 +0700

    chore(first-install): first install and setup

---
+++ BE-KLINIK/src/main/java/alexistdev/com/beklinik/BeKlinikApplication.java
@@ -0,0 +1,13 @@
+package alexistdev.com.beklinik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeKlinikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeKlinikApplication.class, args);
+	}
+
+}
---
+++ BE-KLINIK/src/test/java/alexistdev/com/beklinik/BeKlinikApplicationTests.java
@@ -0,0 +1,13 @@
+package alexistdev.com.beklinik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeKlinikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
